976179e97feccee88aca3d74c6419f90f1c09bf4
mkimyagery <mkimyagery@gmail.com>
2020-12-26 11:23:34 +0100

    new

---
+++ src/test/java/com/uplifter/tests/test1.java
@@ -0,0 +1,11 @@
+package com.uplifter.tests;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+    @Test
+
+    public void test1(){
+        System.out.println("new project");
+    }
+}
