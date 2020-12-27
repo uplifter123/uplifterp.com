402351e605ffd8cfb4414fcb0706707d7bebb7a1
Fatihkesik <fatihkesikfk@gmail.com>
2020-12-27 13:28:52 +0100

    cvcvcv

--- src/test/java/com/uplifter/pages/p.java
+++ src/test/java/com/uplifter/pages/p.java
@@ -3,7 +3,10 @@ package com.uplifter.pages;
 public class p {
 
     public static void main(String[] args) {
+
         System.out.println("kraaaallll");
+
+        int g = 0;
 
         System.out.println("jkbdsjbfdjsab");
     }
