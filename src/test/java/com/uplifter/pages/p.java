2e9b208575d84d80f5e503a3da1a669bb4ccf22f
Fatihkesik <fatihkesikfk@gmail.com>
2020-12-27 13:16:58 +0100

    nbvvcdsds

--- src/test/java/com/uplifter/pages/p.java
+++ src/test/java/com/uplifter/pages/p.java
@@ -3,8 +3,6 @@ package com.uplifter.pages;
 public class p {
 
     public static void main(String[] args) {
-        System.out.println("ongjbgojojvbeb");
-
+        System.out.println("kraaaallll");
     }
-
 }
